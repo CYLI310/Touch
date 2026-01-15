7bb50ed8ed836bcddaeb9d609e90575d120a157f
CYLI310 <chengyangli3041@gmail.com>
2026-01-15 19:46:12 +0800

    Initial Commit

---
+++ Touch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Touch
+//
+//  Created by Justin Li on 2026/1/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Touch/TouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchApp.swift
+//  Touch
+//
+//  Created by Justin Li on 2026/1/15.
+//
+
+import SwiftUI
+
+@main
+struct TouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
